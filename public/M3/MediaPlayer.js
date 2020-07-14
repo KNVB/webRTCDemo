997c3774228c1170f913ea37460cd585076522a0
Roy <dsf@sdfsd.com>
2020-07-14 20:58:02 +0800

    2020-07-14 18:13

--- public/M3/MediaPlayer.js
+++ public/M3/MediaPlayer.js
@@ -1,17 +1,22 @@
 class MediaPlayer {
 	constructor(){
-		let isMaximized = true,muted=true;	
-		
+		var div,muted=true;	
+		var fullScreenCloneList=[],pInPCloneList=[];
+		var btnDiv=document.createElement("div");
+		var cardFooter=document.createElement("div");
 		var container=document.createElement("div");
 		var controlPanel=document.createElement("div");
-		var div=document.createElement("div");
+		
 		var elapseTimeSpan=document.createElement("span");
 		var logger;
-		var maxMinWinLink= document.createElement("a");
-		var muteLink = document.createElement("a");
-		var pInPLink= document.createElement("a");
+	
 		var svgString;
 		var videoTag=document.createElement("video");
+		var upperDiv=document.createElement("div");
+		
+		container.className="card h-100 text-white";
+		container.append(videoTag);
+		container.append(cardFooter);
 		
 		videoTag.className="card-body p-0";
 		videoTag.autoplay=true; 
@@ -20,54 +25,72 @@ class MediaPlayer {
 			elapseTimeSpan.textContent=videoTag.currentTime.toString().toHHMMSS();
 		});		
 		
-		controlPanel.className="align-items-center bg-secondary card-footer d-flex flex-row "; 
-		controlPanel.className+="h4 justify-content-between mb-0 p-1";
-		
-		muteLink.href="javascript:void(0)";
-		muteLink.className="btnlink";
-		muteLink.innerHTML="&#x1f507;"
-		$(muteLink).click(()=>{
+		//Mute button
+		div=document.createElement("div");
+		div.innerHTML="&#x1f507;";
+		div.className="btnlink controlPanel";
+		$(div).click((event)=>{
 			if (muted) {
 				videoTag.muted=false; 
-				muteLink.innerHTML="&#x1f50a;";				
+				$(event.target).html("&#x1f50a;");				
 			} else {
 				videoTag.muted=true;
-				muteLink.innerHTML="&#x1f507;";				
+				$(event.target).html("&#x1f507;");				
 			}
 			muted=!muted;
 		});
+		div.title="Mute";
+		upperDiv.append(div);
+
+		div=document.createElement("div");
+		div.className="elapseTime";
+		div.append(elapseTimeSpan);	
+		upperDiv.append(div);
 		
-		div.append(muteLink);
-		div.append(elapseTimeSpan);
-		controlPanel.append(div);
-		
+		//P in P button
 		svgString ="<svg xmlns=\"http://www.w3.org/2000/svg\"";
 		svgString+="width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" style=\"fill: white;\">";
 		svgString+="<path d=\"M19 7h-8v6h8V7zm2-4H3c-1.1 0-2 .9-2 2v14c0 1.1.9 1.98 2 1.98h18c1.1 0 2-.88 2-1.98V5c0-1.1-.9-2-2-2zm0 16.01H3V4.98h18v14.03z\"/>";
 		svgString+="</svg>";
 		
-		pInPLink.href="javascript:void(0)";
-		pInPLink.className="btnlink";
-		pInPLink.innerHTML=svgString;
-		pInPLink.onclick=pInP;
-		
 		div=document.createElement("div");
-		div.append(pInPLink);
-		controlPanel.append(div);
+		div.innerHTML=svgString;
+		div.className="btnlink";
+		$(div).click((event)=>{
+			pInP();
+		});
+		div.title="P In P";
+		btnDiv.append(div);
 		
+		//Mirror button
 		div=document.createElement("div");
-		maxMinWinLink.href="javascript:void(0)";
-		maxMinWinLink.className="btnlink";
-		maxMinWinLink.innerHTML="&#x26f6;";
-		maxMinWinLink.onclick=maxMinWindow;
+		div.innerHTML="&#x21c4;";
+		div.className="btnlink";
+		$(div).click((event)=>{
+			mirror();	
+		});
+		div.title="Mirror Video";
+		btnDiv.append(div);
 		
-		div.append(maxMinWinLink);
-		controlPanel.append(div);
+		//Max/Min button
+		div=document.createElement("div");
+		div.innerHTML="&#x26f6;";
+		div.className="btnlink maxMin";
+		div.title="Max/Min video";
+		$(div).click((event)=>{
+			maxMin();
+		});
+		btnDiv.append(div);
 		
-		container.className="card h-100 text-white";
-		container.append(videoTag);
-		container.append(controlPanel);
+		btnDiv.className="align-items-center controlPanel d-flex flex-row justify-content-between";
+		upperDiv.className="align-items-center controlPanel d-flex flex-row justify-content-between";
 		
+		controlPanel.className="d-flex flex-column pl-1 pr-1";
+		controlPanel.append(upperDiv);
+		controlPanel.append(btnDiv);		
+				
+		cardFooter.className="bg-secondary card-footer p-0";
+		cardFooter.append(controlPanel);
 //==========================================================================================================						
 		this.addTrack=((track)=>{
 			if (videoTag.srcObject==null){
@@ -94,13 +117,6 @@ class MediaPlayer {
 		this.setLogger=((wl)=>{
 			logger=wl;
 		});
-		this.setMirror=((isMirror)=>{
-			if (isMirror) {
-				$(videoTag).addClass("mirror");
-			} else {
-				$(videoTag).removeClass("mirror");
-			}
-		});
 		this.setSource=((src)=>{
 			setSource(src);
 		});
@@ -108,30 +124,68 @@ class MediaPlayer {
 			setStream(stream);
 		});
 //==========================================================================================================				
+		function clearCloneList(cloneList){
+			var clone;
+			while (cloneList.length>0){
+				clone=cloneList.pop();
+				$(clone).remove();
+			}
+		}
+		function cloneVideo(clone){
+			var cloneVideoTag=$(clone).find("video.card-body")[0];
+			var cloneElapseTimeSpan=$(clone).find("div.elapseTime")[0];
+			cloneVideoTag.currentTime=videoTag.currentTime;
+			cloneVideoTag.srcObject=$(container).find("video.card-body")[0].srcObject;
+			cloneVideoTag.ontimeupdate=(()=>{
+				cloneElapseTimeSpan.textContent=cloneVideoTag.currentTime.toString().toHHMMSS();
+			});
+		}
 		function getVideoTag(){
 			return videoTag;
 		}
-		function maxMinWindow(){
-			$(container).removeClass("pinp");
-			if (!$(container).hasClass("h-100")){
-				$(container).addClass("h-100");
+		function maxMin(){
+			var clone;
+			if (pInPCloneList.length>0){
+				clearCloneList(pInPCloneList);
+				$(container).show();
 			}
-			if (isMaximized) {
-				$(container).addClass("full_screen");
-				maxMinWinLink.innerHTML="&#9634;";
-			} else {
-				$(container).removeClass("full_screen");
-				maxMinWinLink.innerHTML="&#x26f6;";
+			if (fullScreenCloneList.length>0){
+				clearCloneList(fullScreenCloneList);
+				$(container).show();
+			} else {	
+				clone=$(container).clone(true);
+				cloneVideo(clone);
+				$(clone).addClass("full_screen");
+				$(clone).find("div.maxMin").html("&#9634;");
+				$("body").append(clone);
+				$(container).hide();
+				fullScreenCloneList.push(clone);
 			}
-			isMaximized = !isMaximized;
+		}
+		function mirror(){
+			$(videoTag).toggleClass("mirror");
 		}
 		function pInP(){
-			$(container).toggleClass("pinp");
-			$(container).toggleClass("h-100");
-			if ($(container).hasClass("full_screen")){
-				$(container).removeClass("full_screen");
+			var clone;
+			if (fullScreenCloneList.length>0){
+				clearCloneList(fullScreenCloneList);
+				$(container).show();
 			}
-		}
+			if (pInPCloneList.length>0){
+				clearCloneList(pInPCloneList);
+				$(container).show();
+			} else {
+				var clone=$(container).clone(true);
+				cloneVideo(clone);
+				$("body").append(clone);
+				$(clone).addClass("pinp");
+				$(clone).removeClass("h-100");
+				$(clone).removeClass("full_screen");
+				$(clone).draggable();
+				$(container).hide();
+				pInPCloneList.push(clone);
+			}
+		}		
 		function setSource(src){
 			var source=document.createElement("source");
 			source.src=src;
