731cb1b042b010753cb330a14fece2f9e24dce63
Roy <dsf@sdfsd.com>
2020-07-16 23:08:04 +0800

    2020-07-16 23:07

--- public/M3/MediaPlayer.js
+++ public/M3/MediaPlayer.js
@@ -151,7 +151,9 @@ class MediaPlayer {
 			videoTag.srcObject=null;
 			videoTag.srcObject=stream;
 		});
-		
+		this.setTitle=((obj)=>{
+			$(playerOverlayCenter).append(obj);
+		});
 //==========================================================================================================				
 		function clearCloneList(cloneList){
 			var mp;
@@ -224,15 +226,23 @@ class MediaPlayer {
 			$(controlBar).collapse("show");
 		}
 		String.prototype.toHHMMSS = function () {
+			var result="";
 			var sec_num = parseInt(this, 10); // don't forget the second param
 			var hours   = Math.floor(sec_num / 3600);
 			var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
 			var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-			if (hours   < 10) {hours   = "0"+hours;}
+			if (hours>0){
+				result=hours+":";
+				if (hours < 10){
+					result="0"+result;
+				}
+			}
 			if (minutes < 10) {minutes = "0"+minutes;}
 			if (seconds < 10) {seconds = "0"+seconds;}
-			return hours+':'+minutes+':'+seconds;
+			
+			result+=minutes+':'+seconds;
+			return result;
 		}
 	}
 }	
