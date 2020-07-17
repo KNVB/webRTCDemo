c5e4838ccb5c7007845f2567257da46b6a4a6a3c
Roy <dsf@sdfsd.com>
2020-07-17 22:22:09 +0800

    Change icon

--- public/M3/MediaPlayer.js
+++ public/M3/MediaPlayer.js
@@ -17,9 +17,12 @@ class MediaPlayer {
 		var videoTag=document.createElement("video");
 
 		//Max/Min button		
-		maxMinBtn.innerHTML="&#x26f6;";
+		svgString ="<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" style=\"fill: white;\" viewBox=\"0 0 24 24\">";
+		svgString+="<path d=\"M15 3l2.3 2.3-2.89 2.87 1.42 1.42L18.7 6.7 21 9V3zM3 9l2.3-2.3 2.87 2.89 1.42-1.42L6.7 5.3 9 3H3zm6 12l-2.3-2.3 2.89-2.87-1.42-1.42L5.3 17.3 3 15v6zm12-6l-2.3 2.3-2.87-2.89-1.42 1.42 2.89 2.87L15 21h6z\"/>";
+		svgString+="</svg>";
+		maxMinBtn.innerHTML=svgString;
 		maxMinBtn.className="btnlink maxMin";
-		maxMinBtn.title="Max/Min video";
+		maxMinBtn.title="To Full Screen mode";
 		$(maxMinBtn).click((event)=>{
 			doMaxMin(event);
 		});
@@ -33,10 +36,10 @@ class MediaPlayer {
 		});
 		
 		//P in P button
-		svgString ="<svg xmlns=\"http://www.w3.org/2000/svg\"";
-		svgString+="width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" style=\"fill: white;\">";
-		svgString+="<path d=\"M19 7h-8v6h8V7zm2-4H3c-1.1 0-2 .9-2 2v14c0 1.1.9 1.98 2 1.98h18c1.1 0 2-.88 2-1.98V5c0-1.1-.9-2-2-2zm0 16.01H3V4.98h18v14.03z\"/>";
-		svgString+="</svg>";
+		svgString ="<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" style=\"fill: white;\" viewBox=\"0 0 24 24\">";
+		svgString+="<path d=\"M19 11h-8v6h8v-6zm4 8V4.98C23 3.88 22.1 3 21 3H3c-1.1 0-2 .88-2 1.98V19c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2zm-2 .02H3V4.97h18v14.05z\"/>";
+		svgString+="</svg>"
+		
 		pInPBtn.innerHTML=svgString;
 		pInPBtn.className="btnlink";
 		pInPBtn.title="P In P";		
@@ -45,7 +48,11 @@ class MediaPlayer {
 		});
 		
 		//Mute button
-		muteBtn.innerHTML="&#x1f507;";
+		svgString ="<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" style=\"fill: white;\" viewBox=\"0 0 24 24\">";
+		svgString+="<path d=\"M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z\"/>";
+		svgString+="</svg>";
+
+		muteBtn.innerHTML=svgString;
 		muteBtn.className="btnlink";
 		muteBtn.title="Mute";
 		$(muteBtn).click((event)=>{
@@ -96,12 +103,20 @@ class MediaPlayer {
 			videoTag.srcObject.addTrack(track);
 			logger("Remote "+track.kind+" track is added");		
 		});
+		this.collapseControlBar=(()=>{
+			$(controlBar).collapse("hide");
+		});
 		this.getDOMObj=(()=>{
 			return container;
 		});
+
 		this.maxMin=(()=>{
 			$(container).addClass("full_screen");
-			maxMinBtn.innerHTML="&#9634;";
+			svgString ="<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" style=\"fill: white;\" viewBox=\"0 0 24 24\">";
+			svgString+="<path d=\"M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z\"/>";
+			svgString+="</svg>";
+			maxMinBtn.innerHTML=svgString;
+			maxMinBtn.title="Exit From Full Screen mode";
 		});
 		this.pInP=(()=>{
 			$(container).removeClass("h-100");
@@ -154,6 +169,9 @@ class MediaPlayer {
 		this.setTitle=((obj)=>{
 			$(playerOverlayCenter).append(obj);
 		});
+		this.showControlBar=(()=>{
+			$(controlBar).collapse("show");
+		});
 //==========================================================================================================				
 		function clearCloneList(cloneList){
 			var mp;
@@ -169,6 +187,7 @@ class MediaPlayer {
 			mp.setMaxMinHandler(doMaxMin);
 			mp.setPInPHandler(doPInP);
 			mp.setLogger(logger);
+			mp.showControlBar();
 			$("body").append(mp.getDOMObj());
 			return mp;
 		}
@@ -197,10 +216,16 @@ class MediaPlayer {
 			event.stopPropagation();
 			if (muted) {
 				videoTag.muted=false; 
-				$(muteBtn).html("&#x1f50a;");				
+				svgString ="<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" style=\"fill: white;\" viewBox=\"0 0 24 24\">";
+				svgString+="<path d=\"M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z\"/>";
+				svgString+="</svg>";
+				$(muteBtn).html(svgString);				
 			} else {
 				videoTag.muted=true;
-				$(muteBtn).html("&#x1f507;");				
+				svgString ="<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" style=\"fill: white;\" viewBox=\"0 0 24 24\">";
+				svgString+="<path d=\"M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z\"/>";
+				svgString+="</svg>";
+				$(muteBtn).html(svgString);				
 			}
 			muted=!muted;
 		}
@@ -211,7 +236,6 @@ class MediaPlayer {
 			}
 			if (pInPCloneList.length>0){
 				clearCloneList(pInPCloneList);
-				hideControlBar=false;
 
 				$(container).show();
 				doShowControlBar();
