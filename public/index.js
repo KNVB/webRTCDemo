def01e3c2157aae7cfe36c4af489dd1868e141d8
Roy <roy.cstsang@gmail.com>
2020-07-31 00:39:11 +0800

    Changed to use peer.js lib

--- public/index.js
+++ public/index.js
@@ -3,68 +3,42 @@ var https = require('https');
 
 var express = require('express');
 var app = express();
-/*
-var options = {
-  key: fs.readFileSync('.well-known\\acme-challenge\\private.key'),
-  ca: [fs.readFileSync('.well-known\\acme-challenge\\ca_bundle.crt')],
-  cert: fs.readFileSync('.well-known\\acme-challenge\\certificate.crt')
-};
-var serverPort = 443;
-var server = https.createServer(options, app);
-*/
-
 var http = require('http');
-var serverPort = 24;
-server = http.createServer(app);
+var { ExpressPeerServer } = require('peer');
+var httpServerPort = 26;
+var server = http.createServer(app);
+var { v4: uuidV4 } = require('uuid')
+
+var peerServer = ExpressPeerServer(server, {
+  path: '/',
+  proxied: true
+});
 
 var io = require('socket.io')(server);
-var userList=[];
-
+app.set('view engine', 'ejs')
 app.use(express.static('public'));
-io.on('connection', (socket) => {
-	console.log('a user('+socket.id+') connected@'+getTimeString());
-	if (userList.length<2){
-		userList.push(socket.id);
-	} else {
-		socket.emit('rejectConnection','');
-		socket.disconnect(true);
-	}
-	console.log('User list='+JSON.stringify(userList)+'@'+getTimeString());
-	socket.on("closeConnection",()=>{
-		console.log("Close connection request received@"+getTimeString());
-		socket.broadcast.emit("closeConnection", {});
-	});
-	socket.on("send",(req)=>{
-		socket.broadcast.emit("receive", req);
-	});
-	socket.on("sendICECandidate",(req)=>{
-		console.log("Receive an send ICE Candidate request@"+getTimeString());
-		socket.broadcast.emit("receiveICECandidate", req);
-	});
-	socket.on("sendRollDiceResult",(rollDiceResult)=>{
-		socket.broadcast.emit("receiveRollDiceResult",rollDiceResult);
-	});
-	socket.on("sendSDP",(sdp)=>{
-		console.log("Receive an send description request@"+getTimeString());
-		socket.broadcast.emit("receiveSDP", sdp);
-	});
-	socket.on("requestRollDice",(rollDiceResult)=>{
-		socket.broadcast.emit("requestRollDice",rollDiceResult);
-	});
-	socket.on('disconnect', () => {
-		console.log('user ('+socket.id+') disconnected@'+getTimeString());
-		console.log('Before '+JSON.stringify(userList));
-		userList= userList.filter(function(value, index, arr){ return value !== socket.id;});
-		console.log('After '+JSON.stringify(userList));
-	});
-});
+app.use('/peerjs', peerServer);
 
-server.listen(serverPort, function() {
-  console.log('server up and running at %s port', serverPort);
-});
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
 
-function getTimeString(){
-	var date=new Date();
-	var result=date.getHours()+":"+date.getMinutes() +":"+date.getSeconds()+"."+date.getMilliseconds();
-	return result;
-}
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+	console.log("join room:",roomId, userId);	
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+
+server.listen(httpServerPort, function() {
+  console.log('server up and running at %s port', httpServerPort);
+});
