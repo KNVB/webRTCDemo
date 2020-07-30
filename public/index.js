7ae60aef7c84a6908c52ae7d176c8f3342a088a6
SITO3 <bb@cc.com>
2020-07-30 09:32:41 +0800

    Build the mute button synchronization problem

--- public/index.js
+++ public/index.js
@@ -3,7 +3,7 @@ var https = require('https');
 
 var express = require('express');
 var app = express();
-
+/*
 var options = {
   key: fs.readFileSync('.well-known\\acme-challenge\\private.key'),
   ca: [fs.readFileSync('.well-known\\acme-challenge\\ca_bundle.crt')],
@@ -11,12 +11,12 @@ var options = {
 };
 var serverPort = 443;
 var server = https.createServer(options, app);
+*/
 
-/*
 var http = require('http');
 var serverPort = 24;
 server = http.createServer(app);
-*/
+
 var io = require('socket.io')(server);
 var userList=[];
 
@@ -27,9 +27,9 @@ io.on('connection', (socket) => {
 		userList.push(socket.id);
 	} else {
 		socket.emit('rejectConnection','');
-		console.log('User list='+JSON.stringify(userList));
 		socket.disconnect(true);
 	}
+	console.log('User list='+JSON.stringify(userList)+'@'+getTimeString());
 	socket.on("closeConnection",()=>{
 		console.log("Close connection request received@"+getTimeString());
 		socket.broadcast.emit("closeConnection", {});
